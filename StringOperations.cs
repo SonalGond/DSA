09874c0d47b34fe5d85edf454bc22c12e3c5efb3
Sonal Gond <gond.sonal@gmail.com>
2021-11-05 22:57:22 +0530

    String Operations

---
+++ StringOperations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace StringOperations
+{
+        class ValidateStrings
+        {
+
+            static Boolean IsLowerCase(string s)
+            {
+                return s.All(char.IsLower);
+            }
+
+            static Boolean IsUpperCase(string s)
+            {
+                return s.All(char.IsUpper);
+            }
+
+            static Boolean IsDigit(string s)
+            {
+                return s.All(char.IsDigit);
+            }
+
+            static Boolean IsPasswordComplex(string s)
+            {
+                return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine(IsUpperCase("hello"));
+                Console.WriteLine(IsUpperCase("HELLO"));
+
+                Console.WriteLine(IsLowerCase("hello"));
+                Console.WriteLine(IsLowerCase("HELLO"));
+
+                Console.WriteLine(IsDigit("hello"));
+                Console.WriteLine(IsDigit("1234"));
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+
