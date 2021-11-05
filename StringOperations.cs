adfc5d2e25c10812516573194867909ffd1bbd9c
Sonal Gond <gond.sonal@gmail.com>
2021-11-05 23:38:01 +0530

    updated string operations

--- StringOperations.cs
+++ StringOperations.cs
@@ -3,43 +3,54 @@ using System.Linq;
 
 namespace StringOperations
 {
-        class ValidateStrings
+    class ValidateStrings
+    {
+
+        static Boolean IsLowerCase(string s)
         {
+            return s.All(char.IsLower);
+        }
 
-            static Boolean IsLowerCase(string s)
-            {
-                return s.All(char.IsLower);
-            }
+        static Boolean IsUpperCase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
 
-            static Boolean IsUpperCase(string s)
-            {
-                return s.All(char.IsUpper);
-            }
+        static Boolean IsDigit(string s)
+        {
+            return s.All(char.IsDigit);
+        }
 
-            static Boolean IsDigit(string s)
-            {
-                return s.All(char.IsDigit);
-            }
+        static Boolean IsPasswordComplex(string s)
+        {
+            return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+        }
 
-            static Boolean IsPasswordComplex(string s)
-            {
-                return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
-            }
+        static string NormalizeString(string input)
+        {
+            return input.ToLower().Replace(",", "").Trim();
+        }
 
-            static void Main(string[] args)
-            {
-                Console.WriteLine(IsUpperCase("hello"));
-                Console.WriteLine(IsUpperCase("HELLO"));
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsUpperCase("hello"));
+            Console.WriteLine(IsUpperCase("HELLO"));
 
-                Console.WriteLine(IsLowerCase("hello"));
-                Console.WriteLine(IsLowerCase("HELLO"));
+            Console.WriteLine(IsLowerCase("hello"));
+            Console.WriteLine(IsLowerCase("HELLO"));
 
-                Console.WriteLine(IsDigit("hello"));
-                Console.WriteLine(IsDigit("1234"));
+            Console.WriteLine(IsDigit("hello"));
+            Console.WriteLine(IsDigit("1234"));
 
-                Console.ReadLine();
-            }
+            Console.WriteLine(IsPasswordComplex("hello\\"));
+            Console.WriteLine(IsPasswordComplex("1234Hello"));
+
+            Console.WriteLine(NormalizeString("Hello There, BUDDY       "));
+
+            Console.WriteLine(NormalizeString("Hello There, BUDDY       ").Contains("there"));
+            Console.ReadLine();
         }
     }
+}
 
 
